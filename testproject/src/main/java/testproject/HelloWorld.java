44d311dd8b6c0e6a2223194a8aab0915d02cbe0e
Johannes Steck <johannes-steck@gmx.de>
2015-11-19 08:06:03 +0100

    test 2

---
+++ testproject/src/main/java/testproject/HelloWorld.java
@@ -0,0 +1,10 @@
+package testproject;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello Git World!");
+	}
+
+}
